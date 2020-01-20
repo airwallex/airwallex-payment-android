ae549d890c93accd946212d23a263c8338841dff
Evan Liu <evan@interactivelabs.co>
2020-01-20 22:10:15 +0800

    test model

---
+++ airwallex/src/test/java/com/airwallex/android/model/AddressTest.kt
@@ -0,0 +1,29 @@
+package com.airwallex.android.model
+
+import com.airwallex.android.ParcelUtils
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class AddressTest {
+
+    @Test
+    fun builderConstructor() {
+        val address = Address.Builder()
+            .setCountryCode("CN")
+            .setState("Shanghai")
+            .setCity("Shanghai")
+            .setStreet("Pudong District")
+            .setPostcode("100000")
+            .build()
+        assertEquals(address, AddressFixtures.ADDRESS)
+    }
+
+    @Test
+    fun testParcelize() {
+        assertEquals(AddressFixtures.ADDRESS, ParcelUtils.create(AddressFixtures.ADDRESS))
+    }
+
+}
