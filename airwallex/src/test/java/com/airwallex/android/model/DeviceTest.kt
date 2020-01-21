79ab3daac5ea5c4514b80990245827b41ac17a86
Evan Liu <evan@interactivelabs.co>
2020-01-21 15:15:51 +0800

    Fix the lint issues

--- airwallex/src/test/java/com/airwallex/android/model/DeviceTest.kt
+++ airwallex/src/test/java/com/airwallex/android/model/DeviceTest.kt
@@ -1,10 +1,10 @@
 package com.airwallex.android.model
 
 import com.airwallex.android.ParcelUtils
-import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import kotlin.test.assertEquals
 
 @RunWith(RobolectricTestRunner::class)
 class DeviceTest {
