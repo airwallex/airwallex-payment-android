087f375377ec593aea1c8f7ae4e6d617e7d714f4
Evan Liu <evan@interactivelabs.co>
2020-01-21 18:13:25 +0800

    auth url to constant

--- sample/src/main/java/com/airwallex/paymentacceptance/Constants.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/Constants.kt
@@ -2,10 +2,13 @@ package com.airwallex.paymentacceptance
 
 internal object Constants {
 
+    const val AUTH_URL = "https://api-staging.airwallex.com"
+
     const val BASE_URL = "https://staging-pci-api.airwallex.com"
 
     const val API_KEY =
         "9092eb393908b656c2ed8134535b574c30e7a243718a1c08a06b8ea9278919f4550af02cac520e062518028204c1dc54"
+
     const val CLIENT_ID = "DW19XFSMSUq4YPc7xkM4Nw"
 
     // Wechat pay
