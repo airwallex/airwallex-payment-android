eb7d09d3013d73fa58e5f3a32dde4ad111711153
Evan Liu <evan@interactivelabs.co>
2020-01-22 11:42:28 +0800

    wechat pay integration

--- sample/src/main/java/com/airwallex/paymentacceptance/BaseWXPayEntryActivity.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/BaseWXPayEntryActivity.kt
@@ -1,7 +1,6 @@
 package com.airwallex.paymentacceptance
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.tencent.mm.opensdk.modelbase.BaseReq
 import com.tencent.mm.opensdk.modelbase.BaseResp
@@ -21,19 +20,12 @@ abstract class BaseWXPayEntryActivity : AppCompatActivity(), IWXAPIEventHandler 
 
     override fun onResp(resp: BaseResp?) {
         if (resp is PayResp) {
-            val message = when (resp.errCode) {
-                0 -> "Payment successful!"
-                -1 -> "Error during payment"
-                -2 -> "User canceled"
-                else -> "Unknown result"
-            }
-            Toast.makeText(this, message, Toast.LENGTH_SHORT)
-                .show()
+            WXPay.instance.onResp(resp.errCode, resp.errStr)
         }
+        finish()
     }
 
     override fun onReq(req: BaseReq?) {
-        Toast.makeText(this, "Request type: " + req?.type, Toast.LENGTH_SHORT)
-            .show()
+
     }
 }
---
+++ sample/src/main/java/com/airwallex/paymentacceptance/PaymentFlowExtras.kt
@@ -0,0 +1,6 @@
+package com.airwallex.paymentacceptance
+
+object PaymentFlowExtras {
+
+    const val EXTRA_WECHAT_PAYMENT_RESPONSE_CODE: String = "wechat_payment_response_code"
+}
