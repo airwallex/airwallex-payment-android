a12d87e46df484b97026ea0b9d97ef3ef84863f5
Evan Liu <evan@interactivelabs.co>
2020-02-07 15:31:10 +0800

    Fix the crash

--- sample/src/main/java/com/airwallex/paymentacceptance/view/AirwallexTextInputLayout.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/view/AirwallexTextInputLayout.kt
@@ -15,7 +15,7 @@ import com.airwallex.paymentacceptance.R
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
-internal open class AirwallexTextInputLayout constructor(
+internal open class AirwallexTextInputLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet,
     resourceLayout: Int = R.layout.common_text_input_layout
