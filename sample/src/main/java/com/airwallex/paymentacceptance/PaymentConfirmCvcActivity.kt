046550d71875bae4ace5188198c830cc335986ad
Evan Liu <evan@interactivelabs.co>
2020-02-11 11:17:17 +0800

    Move shipping to cart

--- sample/src/main/java/com/airwallex/paymentacceptance/PaymentConfirmCvcActivity.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/PaymentConfirmCvcActivity.kt
@@ -88,17 +88,6 @@ class PaymentConfirmCvcActivity : PaymentBaseActivity() {
 
     private fun startConfirmPaymentIntent(paymentMethod: PaymentMethod, cvc: String) {
         val paymentIntentParams: PaymentIntentParams
-        val device = Device.Builder()
-            .setBrowserInfo("Chrome/76.0.3809.100")
-            .setCookiesAccepted("true")
-            .setDeviceId("IMEI-4432fsdafd31243244fdsafdfd653")
-            .setHostName("www.airwallex.com")
-            .setHttpBrowserEmail("jim631@sina.com")
-            .setHttpBrowserType("chrome")
-            .setIpAddress("123.90.0.1")
-            .setIpNetworkAddress("128.0.0.0")
-            .build()
-
         val paymentMethodOptions: PaymentMethodOptions = PaymentMethodOptions.Builder()
             .setCardOptions(
                 PaymentMethodOptions.CardOptions.Builder()
@@ -114,7 +103,7 @@ class PaymentConfirmCvcActivity : PaymentBaseActivity() {
 
         paymentIntentParams = PaymentIntentParams.Builder()
             .setRequestId(UUID.randomUUID().toString())
-            .setDevice(device)
+            .setDevice(PaymentData.device)
             .setPaymentMethodReference(
                 PaymentMethodReference.Builder()
                     .setId(paymentMethod.id)
