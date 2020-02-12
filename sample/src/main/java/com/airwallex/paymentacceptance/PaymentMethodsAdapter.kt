9c4e641e5576a5ad2ff3a9ad288872f97514d017
Evan Liu <evan@interactivelabs.co>
2020-02-12 18:30:53 +0800

    payment methods sort

--- sample/src/main/java/com/airwallex/paymentacceptance/PaymentMethodsAdapter.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/PaymentMethodsAdapter.kt
@@ -9,9 +9,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.airwallex.android.model.*
 import com.airwallex.android.view.AddPaymentMethodActivity
+import com.airwallex.android.view.CardBrand
 import kotlinx.android.synthetic.main.payment_method_item_card.view.*
 import kotlinx.android.synthetic.main.payment_method_item_footer.view.*
-import kotlinx.android.synthetic.main.payment_method_item_header.view.*
 import java.util.*
 
 class PaymentMethodsAdapter(
@@ -22,45 +22,29 @@ class PaymentMethodsAdapter(
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     enum class ItemViewType(val value: Int) {
-        HEADER(1),
-        ITEM(2),
-        FOOTER(3)
+        CARD(1),
+        FOOTER(2)
     }
 
     override fun getItemViewType(position: Int): Int {
-        return when (position) {
-            0 -> {
-                ItemViewType.HEADER.value
-            }
-            paymentMethods.size - 1 -> {
-                ItemViewType.FOOTER.value
-            }
-            else -> {
-                ItemViewType.ITEM.value
-            }
+        return if (position == paymentMethods.size - 1) {
+            ItemViewType.FOOTER.value
+        } else {
+            ItemViewType.CARD.value
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            ItemViewType.HEADER.value -> {
-                HeaderHolder(
-                    LayoutInflater.from(parent.context)
-                        .inflate(R.layout.payment_method_item_header, parent, false)
-                )
-            }
-            ItemViewType.FOOTER.value -> {
-                FooterHolder(
-                    LayoutInflater.from(parent.context)
-                        .inflate(R.layout.payment_method_item_footer, parent, false)
-                )
-            }
-            else -> {
-                CardHolder(
-                    LayoutInflater.from(parent.context)
-                        .inflate(R.layout.payment_method_item_card, parent, false)
-                )
-            }
+        return if (viewType == ItemViewType.FOOTER.value) {
+            FooterHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.payment_method_item_footer, parent, false)
+            )
+        } else {
+            CardHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.payment_method_item_card, parent, false)
+            )
         }
     }
 
@@ -71,31 +55,8 @@ class PaymentMethodsAdapter(
     @ExperimentalStdlibApi
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
-            is HeaderHolder -> holder.bindView()
             is FooterHolder -> holder.bindView()
             is CardHolder -> holder.bindView(position)
-        }
-    }
-
-    inner class HeaderHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        fun bindView() {
-            itemView.ivChecked.visibility =
-                if (selectedPaymentMethod?.type == PaymentMethodType.WECHAT) View.VISIBLE else View.GONE
-
-            itemView.rlWechatPay.setOnClickListener {
-                if (selectedPaymentMethod?.type == PaymentMethodType.WECHAT) {
-                    return@setOnClickListener
-                }
-                selectedPaymentMethod = PaymentMethod.Builder()
-                    .setType(PaymentMethodType.WECHAT)
-                    .setWechatPayFlow(WechatPayFlow(WechatPayFlowType.INAPP))
-                    .setBilling(PaymentData.billing)
-                    .build()
-
-                (context as PaymentMethodsActivity).invalidateOptionsMenu()
-                notifyDataSetChanged()
-            }
         }
     }
 
@@ -109,14 +70,12 @@ class PaymentMethodsAdapter(
             itemView.tvCardInfo.text =
                 String.format("%s •••• %s", card.brand?.capitalize(Locale.ENGLISH), card.last4)
             when (card.brand) {
-                "visa" -> itemView.ivCardIcon.setImageResource(R.drawable.airwallex_ic_visa)
-                "mastercard" -> itemView.ivCardIcon.setImageResource(R.drawable.airwallex_ic_mastercard)
+                CardBrand.Visa.code -> itemView.ivCardIcon.setImageResource(R.drawable.airwallex_ic_visa)
+                CardBrand.MasterCard.code -> itemView.ivCardIcon.setImageResource(R.drawable.airwallex_ic_mastercard)
             }
             itemView.rlCard.setOnClickListener {
                 val context = context as PaymentMethodsActivity
-                if (selectedPaymentMethod?.type == PaymentMethodType.CARD && method.id == selectedPaymentMethod?.id) {
-                    // No need to update payment method
-                } else {
+                if (selectedPaymentMethod?.type != PaymentMethodType.CARD || method.id != selectedPaymentMethod?.id) {
                     selectedPaymentMethod = PaymentMethod.Builder()
                         .setId(method.id)
                         .setType(PaymentMethodType.CARD)
@@ -143,6 +102,23 @@ class PaymentMethodsAdapter(
                     Store.token,
                     paymentIntent.clientSecret!!
                 )
+            }
+
+            itemView.ivChecked.visibility =
+                if (selectedPaymentMethod?.type == PaymentMethodType.WECHAT) View.VISIBLE else View.GONE
+
+            itemView.rlWechatPay.setOnClickListener {
+                if (selectedPaymentMethod?.type == PaymentMethodType.WECHAT) {
+                    return@setOnClickListener
+                }
+                selectedPaymentMethod = PaymentMethod.Builder()
+                    .setType(PaymentMethodType.WECHAT)
+                    .setWechatPayFlow(WechatPayFlow(WechatPayFlowType.INAPP))
+                    .setBilling(PaymentData.billing)
+                    .build()
+
+                (context as PaymentMethodsActivity).invalidateOptionsMenu()
+                notifyDataSetChanged()
             }
         }
     }
