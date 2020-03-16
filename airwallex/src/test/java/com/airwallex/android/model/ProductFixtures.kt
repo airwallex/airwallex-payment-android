d4eefbfad4185a815f9f0e1d8161b2cfda503ce7
Evan Liu <evan@interactivelabs.co>
2020-03-16 15:35:43 +0800

    More models test

---
+++ airwallex/src/test/java/com/airwallex/android/model/ProductFixtures.kt
@@ -0,0 +1,21 @@
+package com.airwallex.android.model
+
+import com.airwallex.android.AirwallexPlugins
+
+internal object ProductFixtures {
+    val PRODUCT: Product = AirwallexPlugins.gson.fromJson(
+        """
+        {
+            "code": "123",
+            "name": "AirPods Pro",
+            "desc": "Buy AirPods Pro, per month with trade-in",
+            "sku": "piece",
+            "type": "Free engraving",
+            "unit_price": 399.00,
+            "url": "www.aircross.com",
+            "quantity": 1
+        }
+        """.trimIndent(),
+        Product::class.java
+    )
+}
---
+++ airwallex/src/test/java/com/airwallex/android/model/ProductTest.kt
@@ -0,0 +1,30 @@
+package com.airwallex.android.model
+
+import com.airwallex.android.ParcelUtils
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+class ProductTest {
+    @Test
+    fun builderConstructor() {
+        val product = Product.Builder()
+            .setCode("123")
+            .setName("AirPods Pro")
+            .setDesc("Buy AirPods Pro, per month with trade-in")
+            .setSku("piece")
+            .setType("Free engraving")
+            .setUnitPrice(399.00)
+            .setUrl("www.aircross.com")
+            .setQuantity(1)
+            .build()
+        assertEquals(product, ProductFixtures.PRODUCT)
+    }
+
+    @Test
+    fun testParcelable() {
+        assertEquals(ProductFixtures.PRODUCT, ParcelUtils.create(ProductFixtures.PRODUCT))
+    }
+}
