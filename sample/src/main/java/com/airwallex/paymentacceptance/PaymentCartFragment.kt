3ced4ac292e395a77551947535a2d66c38292b2f
Evan Liu <evan@interactivelabs.co>
2020-03-19 19:24:26 +0800

    Remove unused resources

--- sample/src/main/java/com/airwallex/paymentacceptance/PaymentCartFragment.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/PaymentCartFragment.kt
@@ -2,7 +2,6 @@ package com.airwallex.paymentacceptance
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
