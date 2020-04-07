f3365aa35a33a9055d41374846b472bce31e359a
Evan Liu <evan@interactivelabs.co>
2020-04-07 10:30:40 +0800

    Remove `ConfirmPaymentIntentType`

--- airwallex/src/main/java/com/airwallex/android/ConfirmPaymentIntentParams.kt
+++ airwallex/src/main/java/com/airwallex/android/ConfirmPaymentIntentParams.kt
@@ -1,40 +1,45 @@
 package com.airwallex.android
 
 import com.airwallex.android.model.ObjectBuilder
+import com.airwallex.android.model.PaymentMethod
 
 data class ConfirmPaymentIntentParams internal constructor(
-    val type: ConfirmPaymentIntentType,
     override val paymentIntentId: String,
     override val clientSecret: String,
     /**
      * optional, the ID of a Customer.
      */
-    val customerId: String?
+    val customerId: String?,
+    /**
+     * ID of the [PaymentMethod]
+     */
+    val paymentMethodId: String?
 ) : AbstractPaymentIntentParams(paymentIntentId = paymentIntentId, clientSecret = clientSecret) {
 
     class Builder(
-        private val type: ConfirmPaymentIntentType = ConfirmPaymentIntentType.WECHAT,
         private val paymentIntentId: String,
         private val clientSecret: String
     ) : ObjectBuilder<ConfirmPaymentIntentParams> {
 
         private var customerId: String? = null
 
+        private var paymentMethodId: String? = null
+
         fun setCustomerId(customerId: String?): Builder = apply {
             this.customerId = customerId
         }
 
+        fun setPaymentMethodId(paymentMethodId: String?): Builder = apply {
+            this.paymentMethodId = paymentMethodId
+        }
+
         override fun build(): ConfirmPaymentIntentParams {
             return ConfirmPaymentIntentParams(
-                type = type,
                 paymentIntentId = paymentIntentId,
                 clientSecret = clientSecret,
-                customerId = customerId
+                customerId = customerId,
+                paymentMethodId = paymentMethodId
             )
         }
-    }
-
-    enum class ConfirmPaymentIntentType {
-        WECHAT
     }
 }
