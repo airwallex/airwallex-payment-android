6ca1e28541e2a268a6bd04da7e571340da575828
Evan Liu <evan@interactivelabs.co>
2020-04-16 15:34:01 +0800

    Replace new aar

--- airwallex/src/main/java/com/airwallex/android/SecurityConnector.java
+++ airwallex/src/main/java/com/airwallex/android/SecurityConnector.java
@@ -3,5 +3,5 @@ package com.airwallex.android;
 import android.content.Context;
 
 public interface SecurityConnector {
-    void retrieveSecurityToken(Context applicationContext, AirwallexSecurityConnector.TrustDefenderListener trustDefenderListener);
+    void retrieveSecurityToken(Context applicationContext, String paymentIntentId, String customerId, AirwallexSecurityConnector.TrustDefenderListener trustDefenderListener);
 }
