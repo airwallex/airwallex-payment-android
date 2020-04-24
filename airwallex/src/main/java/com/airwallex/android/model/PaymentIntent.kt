4ca5616e490ff563981ea08bec8a16de213b30cc
Evan Liu <evan@interactivelabs.co>
2020-04-24 11:14:23 +0800

    Guide Update

--- airwallex/src/main/java/com/airwallex/android/model/PaymentIntent.kt
+++ airwallex/src/main/java/com/airwallex/android/model/PaymentIntent.kt
@@ -104,7 +104,7 @@ data class PaymentIntent internal constructor(
      * Client secret for browser or app
      */
     @SerializedName("client_secret")
-    val clientSecret: String?,
+    val clientSecret: String,
 
     /**
      * Next action for the payment intent
