77e401215738338b9b24766795a13b70b3f2e7b0
Evan <evan@interactivelabs.co>
2020-05-14 11:30:42 +0800

    Add toolbar for three ds 1.0

--- airwallex/src/main/java/com/airwallex/android/ThreeDSecureActivity.kt
+++ airwallex/src/main/java/com/airwallex/android/ThreeDSecureActivity.kt
@@ -22,12 +22,20 @@ internal class ThreeDSecureActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         if (threeDSecureLookup.dsData.version?.startsWith("1.") == true) {
+            // 3DS 1.0
             if (threeDSecureLookup.payload == null || threeDSecureLookup.acsUrl == null) {
                 finishThreeDSecure1(null, false)
                 return
             }
 
             setContentView(R.layout.activity_threeds)
+
+            setSupportActionBar(toolbar)
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.airwallex_ic_back)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+            toolbar.setNavigationOnClickListener { onBackPressed() }
+
             webView.webViewClient =
                 ThreeDSecureWebViewClient(object : ThreeDSecureWebViewClient.Callbacks {
                     override fun onWebViewConfirmation(payload: String) {
@@ -42,13 +50,14 @@ internal class ThreeDSecureActivity : AppCompatActivity() {
             webView.webChromeClient = WebChromeClient()
 
             val payload = threeDSecureLookup.payload
-            val termUrl = "https://term-url/"
+            val termUrl = "https://term-url/aaa"
             val acsUrl = threeDSecureLookup.acsUrl
             val postData = "&PaReq=" + URLEncoder.encode(payload, "UTF-8")
                 .toString() + "&TermUrl=" + URLEncoder.encode(termUrl, "UTF-8")
 
             webView.postUrl(acsUrl, postData.toByteArray())
         } else {
+            // 3DS 2.0
             Cardinal.getInstance().cca_continue(
                 threeDSecureLookup.transactionId,
                 threeDSecureLookup.payload,
@@ -64,6 +73,7 @@ internal class ThreeDSecureActivity : AppCompatActivity() {
         super.onBackPressed()
     }
 
+    // 3DS 2.0
     private fun finishThreeDSecure2(validateResponse: ValidateResponse?, jwt: String?) {
         val result = Intent()
         result.putExtra(EXTRA_THREE_D_JWT, jwt)
@@ -80,6 +90,7 @@ internal class ThreeDSecureActivity : AppCompatActivity() {
         finish()
     }
 
+    // 3DS 1.0
     private fun finishThreeDSecure1(payload: String? = null, cancel: Boolean) {
         val result = Intent()
         result.putExtra(EXTRA_THREE_PAYLOAD, payload)
