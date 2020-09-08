de8ea83b017bbdcb4dbd41d8dce5a8e97475b67f
Evan <evan@interactivelabs.co>
2020-09-08 09:51:34 +0800

    AirwallexException code refactor

--- airwallex/src/main/java/com/airwallex/android/exception/WebViewConnectionException.kt
+++ airwallex/src/main/java/com/airwallex/android/exception/WebViewConnectionException.kt
@@ -1,6 +1,8 @@
 package com.airwallex.android.exception
 
+import com.airwallex.android.model.AirwallexError
+
 class WebViewConnectionException @JvmOverloads constructor(
-    message: String?,
+    message: String,
     e: Throwable? = null
-) : AirwallexException(null, message, null, 0, e)
+) : AirwallexException(AirwallexError(message = message), null, 0, message, e)
