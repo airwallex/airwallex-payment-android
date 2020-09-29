113c396d8cac8540f08aa4f7b850778b08db835c
Evan <evan@interactivelabs.co>
2020-09-29 17:37:30 +0800

    clean code

--- sample/src/main/java/com/airwallex/paymentacceptance/ApiFactory.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/ApiFactory.kt
@@ -10,6 +10,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 import java.util.concurrent.TimeUnit
+import kotlin.jvm.Throws
 
 internal class ApiFactory internal constructor(private val baseUrl: String) {
 
