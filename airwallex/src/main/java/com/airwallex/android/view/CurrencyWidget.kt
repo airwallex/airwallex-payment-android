a8fb2e7a3dad1244486e03999ead5107535921b3
Evan <evan@interactivelabs.co>
2020-09-29 17:29:38 +0800

    Support currency icon

---
+++ airwallex/src/main/java/com/airwallex/android/view/CurrencyWidget.kt
@@ -0,0 +1,32 @@
+package com.airwallex.android.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.content.res.ResourcesCompat
+import com.airwallex.android.CurrencyUtils
+import com.airwallex.android.R
+import kotlinx.android.synthetic.main.widget_currency.view.*
+import java.math.BigDecimal
+import java.util.*
+
+internal class CurrencyWidget(context: Context, attrs: AttributeSet) :
+    FrameLayout(context, attrs) {
+
+    init {
+        View.inflate(context, R.layout.widget_currency, this)
+    }
+
+    fun updateCurrency(currency: String, amount: BigDecimal) {
+        tv_currency.text = currency
+        tv_price.text = String.format("%s%.2f", CurrencyUtils.getCurrencySymbol(currency), amount)
+
+        try {
+            val resourceId: Int = resources.getIdentifier(currency.toLowerCase(Locale.US), "drawable", context.packageName)
+            icon.setImageDrawable(ResourcesCompat.getDrawable(resources, resourceId, null))
+        } catch (e: Exception) {
+            icon.visibility = View.GONE
+        }
+    }
+}
