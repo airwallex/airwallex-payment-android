7337021dc05e270d8952b3f895fe8efe61a1eaf1
Evan <evan@interactivelabs.co>
2020-10-07 15:15:58 +0800

    Format price

--- airwallex/src/main/java/com/airwallex/android/CurrencyUtils.kt
+++ airwallex/src/main/java/com/airwallex/android/CurrencyUtils.kt
@@ -1,14 +1,30 @@
 package com.airwallex.android
 
+import java.math.BigDecimal
+import java.text.DecimalFormat
+import java.text.NumberFormat
 import java.util.*
 
 object CurrencyUtils {
+
+    fun formatPrice(
+        currency: String,
+        amount: BigDecimal
+    ): String {
+        val currencyFormat = NumberFormat.getCurrencyInstance()
+        val decimalFormatSymbols = (currencyFormat as DecimalFormat)
+            .decimalFormatSymbols
+        val symbol = getCurrencySymbol(currency)
+        decimalFormatSymbols.currencySymbol = symbol
+        currencyFormat.decimalFormatSymbols = decimalFormatSymbols
+        return currencyFormat.format(amount)
+    }
 
     private val currencyLocaleMap: TreeMap<Currency, Locale> by lazy {
         TreeMap(Comparator { c1, c2 -> c1.currencyCode.compareTo(c2.currencyCode) })
     }
 
-    fun getCurrencySymbol(currencyCode: String?): String {
+    private fun getCurrencySymbol(currencyCode: String?): String {
         val currency = Currency.getInstance(currencyCode)
         if (currencyLocaleMap.containsKey(currency)) {
             return currency.getSymbol(currencyLocaleMap[currency])
