67c5050dd6375f30592333c2c7935faffe8853a4
Hector Huang <hector.huang@airwallex.com>
2022-08-31 13:25:43 +0800

    chore: add tests

--- airwallex/src/test/java/com/airwallex/android/view/CardExpiryEditTextTest.kt
+++ airwallex/src/test/java/com/airwallex/android/view/CardExpiryEditTextTest.kt
@@ -3,11 +3,14 @@ package com.airwallex.android.view
 import androidx.appcompat.view.ContextThemeWrapper
 import androidx.test.core.app.ApplicationProvider
 import com.airwallex.android.ui.R
+import io.mockk.*
+import org.junit.After
+import org.junit.Before
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 @RunWith(RobolectricTestRunner::class)
 class CardExpiryEditTextTest {
@@ -18,11 +21,17 @@ class CardExpiryEditTextTest {
         R.style.AirwallexDefaultTheme
     )
 
-    @BeforeTest
+    @Before
     fun setup() {
         cardExpiryEditText =
             CardExpiryEditText(context)
         cardExpiryEditText.setText("")
+        mockkObject(ExpiryDateUtils)
+    }
+
+    @After
+    fun unmock() {
+        unmockkAll()
     }
 
     @Test
@@ -45,5 +54,22 @@ class CardExpiryEditTextTest {
         cardExpiryEditText.append("2")
         cardExpiryEditText.append("3")
         assertEquals("12/3", cardExpiryEditText.text.toString())
+    }
+
+    @Test
+    fun testErrorCallbackWhenCardExpires() {
+        every { ExpiryDateUtils.isExpiryDateValid(any(), any()) } returns false
+        var hasError = false
+        cardExpiryEditText.errorCallback = { hasError = it }
+        cardExpiryEditText.setText("12/21")
+        assertTrue(hasError)
+    }
+
+    @Test
+    fun testErrorCallbackWhenYearLengthIsNotTwo() {
+        var hasError = false
+        cardExpiryEditText.errorCallback = { hasError = it }
+        cardExpiryEditText.setText("12/2023")
+        assertTrue(hasError)
     }
 }
