b4546bd7134bba80c21faa5d227da5a7e6766cde
Hector Huang <hector.huang@airwallex.com>
2022-11-23 14:30:01 +0800

    chore: exclude demo app from unit test and add 3ds tests for card

--- card/src/test/java/com/airwallex/android/card/CardComponentTest.kt
+++ card/src/test/java/com/airwallex/android/card/CardComponentTest.kt
@@ -1,12 +1,81 @@
 package com.airwallex.android.card
 
+import android.app.Activity
+import android.content.Context
+import com.airwallex.android.core.Airwallex
+import com.airwallex.android.core.AirwallexApiRepository
+import com.airwallex.android.core.AirwallexPaymentManager
+import com.airwallex.android.core.CardNextActionModel
+import com.airwallex.android.core.model.NextAction
+import com.airwallex.android.threedsecurity.ThreeDSecurityManager
+import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.unmockkObject
+import io.mockk.verify
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
+import java.math.BigDecimal
 import kotlin.test.assertNotNull
 
 class CardComponentTest {
+    lateinit var component: CardComponent
+    lateinit var activity: Activity
+    lateinit var context: Context
+    lateinit var listener: Airwallex.PaymentResultListener
+
+    @Before
+    fun setUp() {
+        mockkObject(ThreeDSecurityManager)
+        activity = mockk()
+        context = mockk()
+        listener = mockk(relaxed = true)
+        component = CardComponent()
+    }
+
+    @After
+    fun unmockStatics() {
+        unmockkObject(ThreeDSecurityManager)
+    }
 
     @Test
-    fun test() {
+    fun `test Provider`() {
         assertNotNull(CardComponent.PROVIDER)
+    }
+
+    @Test
+    fun `test handlePaymentIntentResponse when next action is redirect with card model`() {
+        val redirectAction = NextAction(
+            type = NextAction.NextActionType.REDIRECT_FORM,
+            data = null,
+            dcc = null,
+            url = null,
+            method = null
+        )
+        val cardModel = CardNextActionModel(
+            fragment = null,
+            activity = activity,
+            paymentManager = AirwallexPaymentManager(AirwallexApiRepository()),
+            clientSecret = "tqj9uJlZZ8NIFEM_dpZb2DXbGkQ==",
+            device = null,
+            paymentIntentId = "int_hkdmr7v9rg1j58ky8re",
+            currency = "CNY",
+            amount = BigDecimal.TEN
+        )
+        handlePaymentIntentResponse(redirectAction, cardModel)
+        verify(exactly = 1) {
+            ThreeDSecurityManager.handleThreeDSFlow("id", activity, redirectAction, cardModel, listener)
+        }
+    }
+
+    private fun handlePaymentIntentResponse(action: NextAction? = null, model: CardNextActionModel? = null) {
+        component.handlePaymentIntentResponse(
+            paymentIntentId = "id",
+            nextAction = action,
+            activity = activity,
+            applicationContext = context,
+            cardNextActionModel = model,
+            listener = listener
+        )
     }
 }
