84925753a4121c1a4b3267f2032dad78a4c5d99b
passy.ruan <passy.ruan@airwallex.com>
2024-06-28 18:13:40 +0800

    chore:remove static variables that may cause memory leaks.

--- components-core/src/main/java/com/airwallex/android/core/AirwallexShippingStatus.kt
+++ components-core/src/main/java/com/airwallex/android/core/AirwallexShippingStatus.kt
@@ -1,9 +1,12 @@
 package com.airwallex.android.core
 
+import com.airwallex.android.core.exception.AirwallexException
 import com.airwallex.android.core.model.Shipping
 
 sealed class AirwallexShippingStatus {
     data class Success(val shipping: Shipping) : AirwallexShippingStatus()
+    // shipping failure
+    data class Failure(val exception: AirwallexException) : AirwallexShippingStatus()
 
     object Cancel : AirwallexShippingStatus()
 }
