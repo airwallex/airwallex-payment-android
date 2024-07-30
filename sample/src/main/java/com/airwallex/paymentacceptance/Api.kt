d39d7587a4f90ff119128271e354ad37e969ddfb
passy.ruan <passy.ruan@airwallex.com>
2024-07-30 14:35:47 +0800

    chore:Update the sample to use the generate_client_secret API to generate the client secret in recurring mode.

--- sample/src/main/java/com/airwallex/paymentacceptance/Api.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/Api.kt
@@ -1,8 +1,11 @@
 package com.airwallex.paymentacceptance
 
-import retrofit2.Call
 import okhttp3.ResponseBody
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface Api {
 
