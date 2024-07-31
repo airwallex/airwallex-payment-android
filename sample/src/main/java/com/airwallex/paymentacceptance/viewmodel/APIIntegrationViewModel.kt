d073c6f0ce1afc50ef3309d319272253247c0641
passy.ruan <passy.ruan@airwallex.com>
2024-07-31 14:10:15 +0800

    chore:add UI integration logic

--- sample/src/main/java/com/airwallex/paymentacceptance/viewmodel/APIIntegrationViewModel.kt
+++ sample/src/main/java/com/airwallex/paymentacceptance/viewmodel/APIIntegrationViewModel.kt
@@ -1,7 +1,18 @@
 package com.airwallex.paymentacceptance.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.airwallex.android.core.AirwallexCheckoutMode
 
 class APIIntegrationViewModel: ViewModel() {
+
+    private var checkoutMode = AirwallexCheckoutMode.PAYMENT
+
+    fun updateCheckoutModel(mode: Int) {
+        checkoutMode = when (mode) {
+            1 -> AirwallexCheckoutMode.RECURRING
+            2 -> AirwallexCheckoutMode.RECURRING_WITH_INTENT
+            else -> AirwallexCheckoutMode.PAYMENT
+        }
+    }
 
 }
