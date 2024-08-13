fc26d965ceffb7dac6659f8d007775f1aa6be7c2
passy.ruan <passy.ruan@airwallex.com>
2024-08-13 16:13:22 +0800

    Revert "chore: modify the retrieval logic for IsReadyToPay."
    This reverts commit 93ff640ada781ee02d9b36afec91dc15f9dd8334.

--- googlepay/src/main/java/com/airwallex/android/googlepay/GooglePayComponentProvider.kt
+++ googlepay/src/main/java/com/airwallex/android/googlepay/GooglePayComponentProvider.kt
@@ -63,8 +63,9 @@ class GooglePayComponentProvider : ActionComponentProvider<GooglePayComponent> {
             AirwallexLogger.error("GooglePayComponentProvider requestIsReadyToPay: resultCode = $resultCode")
             return false
         }
-        AirwallexLogger.info("GooglePayComponentProvider requestIsReadyToPay: skipIsReadyToPay = ${options.skipIsReadyToPay}")
-        if (options.skipIsReadyToPay == true) return true
+        val skipIsReadyToPay = session.googlePayOptions?.skipIsReadyToPay ?: false
+        AirwallexLogger.info("GooglePayComponentProvider requestIsReadyToPay: skipIsReadyToPay = $skipIsReadyToPay")
+        if (skipIsReadyToPay) return true
         val request = IsReadyToPayRequest.fromJson(isReadyToPayJson.toString())
         val task = paymentsClient.isReadyToPay(request)
         return suspendCoroutine { cont ->
