f1ed1885a62c1fc0c479e5d6a9c2f86da203b7af
passy.ruan <passy.ruan@airwallex.com>
2024-08-14 13:57:13 +0800

    chore: standardize Naming and Code Formatting

--- components-core/src/main/java/com/airwallex/android/core/AirwallexCallback.kt
+++ components-core/src/main/java/com/airwallex/android/core/AirwallexCallback.kt
@@ -1,6 +1,8 @@
 package com.airwallex.android.core
 
+import com.airwallex.android.core.exception.AirwallexException
+
 interface AirwallexCallback<T> {
     fun onSuccess(result: T)
-    fun onFailure(error: Throwable?)
+    fun onFailure(error: AirwallexException?)
 }
