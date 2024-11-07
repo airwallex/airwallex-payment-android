fb1254575797ac6c06637f684da3c2e6731351b8
passy.ruan <passy.ruan@airwallex.com>
2024-11-07 11:29:22 +0800

    chore: fix the unit test failures caused by the expiration.

--- airwallex/src/test/java/com/airwallex/android/view/CardWidgetTest.kt
+++ airwallex/src/test/java/com/airwallex/android/view/CardWidgetTest.kt
@@ -47,7 +47,7 @@ class CardWidgetTest {
         cardNameTextInputLayout.value = "aaa"
         cardNumberTextInputLayout.value = "4242424242424242"
         cvcTextInputLayout.value = "123"
-        expiryTextInputLayout.value = "1024"
+        expiryTextInputLayout.value = "1090"
         assertEquals(true, cardWidget.isValid)
 
         cardNameTextInputLayout.value = "aaa"
@@ -65,14 +65,14 @@ class CardWidgetTest {
         cardNameTextInputLayout.value = "aaa"
         cardNumberTextInputLayout.value = "4242424242424242"
         cvcTextInputLayout.value = "123"
-        expiryTextInputLayout.value = "1024"
+        expiryTextInputLayout.value = "1090"
         assertEquals(
             PaymentMethod.Card.Builder()
                 .setName("aaa")
                 .setNumber("4242424242424242")
                 .setCvc("123")
                 .setExpiryMonth("10")
-                .setExpiryYear("2024")
+                .setExpiryYear("2090")
                 .build(),
             cardWidget.paymentMethodCard
         )
