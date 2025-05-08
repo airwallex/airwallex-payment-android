9a6011d54472451f21952bbb706c96986a2fa0b1
Jia Ma <jia.ma@airwallex.com>
2025-05-08 18:39:06 +0800

    polish

--- airwallex/src/main/java/com/airwallex/android/view/composables/PaymentMethodsHorizontalList.kt
+++ airwallex/src/main/java/com/airwallex/android/view/composables/PaymentMethodsHorizontalList.kt
@@ -8,26 +8,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.airwallex.android.core.model.AvailablePaymentMethodType
-import com.airwallex.android.core.model.PaymentConsent
 
 @Composable
 internal fun PaymentMethodsHorizontalList(
-    availablePaymentConsents: List<PaymentConsent>,
     availablePaymentMethodTypes: List<AvailablePaymentMethodType>,
 ) {
     LazyRow(modifier = Modifier.padding(horizontal = 24.dp)) {
-        availablePaymentConsents.forEachIndexed { index, paymentConsent ->
-            item(key = "consent_$index") {
-                Spacer(modifier = Modifier.width(12.dp))
-
-                PaymentConsentCard(
-                    paymentConsent = paymentConsent,
-                    onClick = {}, //TODO
-                )
-
-                Spacer(modifier = Modifier.width(12.dp))
-            }
-        }
         availablePaymentMethodTypes.forEachIndexed { index, availablePaymentMethodType ->
             item(key = "payment_method_$index") {
                 Spacer(modifier = Modifier.width(12.dp))
