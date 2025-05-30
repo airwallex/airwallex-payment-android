23e084049aa94b862b58ab4c1f76a1d987f6d8d3
Jia Ma <jia.ma@airwallex.com>
2025-05-30 09:35:35 +0800

    polish

--- components-core/src/main/java/com/airwallex/android/core/Airwallex.kt
+++ components-core/src/main/java/com/airwallex/android/core/Airwallex.kt
@@ -557,7 +557,7 @@ class Airwallex internal constructor(
                     if (response.nextAction == null) {
                         listener.onCompleted(
                             AirwallexPaymentStatus.Success(
-                                "",
+                                paymentIntentId,
                                 response.id
                             )
                         )
