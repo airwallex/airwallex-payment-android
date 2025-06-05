5f4acfae3aa3ae0d10a26864b6c403dc886b8323
Jia Ma <jia.ma@airwallex.com>
2025-06-05 14:59:49 +0800

    1. address comment 2. add coverage

--- components-core/src/test/java/com/airwallex/android/core/util/CardUtilsTest.kt
+++ components-core/src/test/java/com/airwallex/android/core/util/CardUtilsTest.kt
@@ -61,4 +61,60 @@ class CardUtilsTest {
     fun `test maxCardNumberLength`() {
         assertEquals(CardUtils.maxCardNumberLength, 19)
     }
+
+    @Test
+    fun `test formatCardNumber for Visa`() {
+        // Test full length
+        assertEquals("4242 4242 4242 4242", CardUtils.formatCardNumber("4242424242424242", CardBrand.Visa))
+        // Test partial number
+        assertEquals("4242 42", CardUtils.formatCardNumber("424242", CardBrand.Visa))
+        // Test with spaces in input
+        assertEquals("4242 4242 4242 4242", CardUtils.formatCardNumber("4242 4242 4242 4242", CardBrand.Visa))
+        // Test empty string
+        assertEquals("", CardUtils.formatCardNumber("", CardBrand.Visa))
+    }
+
+    @Test
+    fun `test formatCardNumber for Amex`() {
+        // Test full length
+        assertEquals("3782 822463 10005", CardUtils.formatCardNumber("378282246310005", CardBrand.Amex))
+        // Test partial number (before first space)
+        assertEquals("3782", CardUtils.formatCardNumber("3782", CardBrand.Amex))
+        // Test partial number (after first space, before second space)
+        assertEquals("3782 822463", CardUtils.formatCardNumber("3782822463", CardBrand.Amex))
+    }
+
+    @Test
+    fun `test formatCardNumber for MasterCard`() {
+        // Test full length
+        assertEquals("5555 5555 5555 4444", CardUtils.formatCardNumber("5555555555554444", CardBrand.MasterCard))
+        // Test partial number
+        assertEquals("5555 5555 55", CardUtils.formatCardNumber("5555555555", CardBrand.MasterCard))
+    }
+
+    @Test
+    fun `test formatCardNumber for UnionPay`() {
+        // Test full length (19 digits)
+        assertEquals("6212 3188 8888 8888888", CardUtils.formatCardNumber("6212318888888888888", CardBrand.UnionPay))
+        // Test partial number
+        assertEquals("6212 3188 88", CardUtils.formatCardNumber("6212318888", CardBrand.UnionPay))
+    }
+
+    @Test
+    fun `test formatCardNumber for JCB`() {
+        // Test full length
+        assertEquals("3540 1234 5678 9012", CardUtils.formatCardNumber("3540123456789012", CardBrand.JCB))
+    }
+
+    @Test
+    fun `test formatCardNumber for Discover`() {
+        // Test full length
+        assertEquals("6011 6011 6011 6611", CardUtils.formatCardNumber("6011601160116611", CardBrand.DISCOVER))
+    }
+
+    @Test
+    fun `test formatCardNumber for Diners`() {
+        // Test full length (14 digits)
+        assertEquals("3643 893643 8936", CardUtils.formatCardNumber("36438936438936", CardBrand.DINERS))
+    }
 }
