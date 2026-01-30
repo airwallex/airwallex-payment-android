b554cd90be21d032b18588682a4667daa5e4ba99
Ferdian Sulaiman <ferdian.sulaiman@airwallex.com>
2026-01-30 16:18:58 +0800

    [APAM-509] AnalyticsConstants

---
+++ airwallex/src/main/java/com/airwallex/android/view/util/AnalyticsConstants.kt
@@ -0,0 +1,13 @@
+package com.airwallex.android.view.util
+
+object AnalyticsConstants {
+    // event names
+    const val EVENT_PAYMENT_CANCELLED = "payment_canceled"
+    const val EVENT_PAYMENT_LAUNCHED = "payment_launched"
+
+    // page names
+    const val CARD_PAYMENT_VIEW: String = "card_payment_view"
+
+    // extras
+    const val SUPPORTED_SCHEMES: String = "supportedSchemes"
+}
