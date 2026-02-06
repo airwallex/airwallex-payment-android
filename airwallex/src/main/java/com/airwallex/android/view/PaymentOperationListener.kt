3d5640c6eb2575d5c528ea0780a96fc2ea475fc4
Ferdian Sulaiman <ferdian.sulaiman@airwallex.com>
2026-02-06 14:07:59 +0800

    [APAM-532] context instead of activity to support fragment & composable

--- airwallex/src/main/java/com/airwallex/android/view/PaymentOperationListener.kt
+++ airwallex/src/main/java/com/airwallex/android/view/PaymentOperationListener.kt
@@ -1,6 +1,8 @@
 package com.airwallex.android.view
 
-import androidx.activity.ComponentActivity
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import androidx.appcompat.app.AlertDialog
 import com.airwallex.android.core.AirwallexPaymentStatus
 import com.airwallex.android.ui.R
@@ -8,17 +10,26 @@ import com.airwallex.android.ui.R
 interface PaymentOperationListener {
     fun onLoadingStateChanged(isLoading: Boolean)
     fun onPaymentResult(status: AirwallexPaymentStatus)
-    fun onError(exception: Throwable, activity: ComponentActivity) {
-        if (!activity.isFinishing) {
-            AlertDialog.Builder(activity)
-                .setTitle("")
-                .setMessage(exception.message ?: exception.toString())
-                .setCancelable(false)
-                .setPositiveButton(R.string.airwallex_okay) { dialogInterface, _ ->
-                    dialogInterface.dismiss()
+    fun onError(exception: Throwable, context: Context) {
+        var currentContext: Context? = context
+        while (currentContext is ContextWrapper) {
+            if (currentContext is Activity) {
+                if (currentContext.isFinishing) {
+                    return
                 }
-                .create()
-                .show()
+                break
+            }
+            currentContext = currentContext.baseContext
         }
+
+        AlertDialog.Builder(context)
+            .setTitle("")
+            .setMessage(exception.message ?: exception.toString())
+            .setCancelable(false)
+            .setPositiveButton(R.string.airwallex_okay) { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+            .create()
+            .show()
     }
 }
