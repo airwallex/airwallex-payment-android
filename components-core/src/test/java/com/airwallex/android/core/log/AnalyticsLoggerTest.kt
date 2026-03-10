f13b1b9fa56274860d87c31bed19ca98f5a17bdf
Ferdian Sulaiman <ferdian.sulaiman@airwallex.com>
2026-03-10 16:03:52 +0800

    [APAM-565] fix unit test

--- components-core/src/test/java/com/airwallex/android/core/log/AnalyticsLoggerTest.kt
+++ components-core/src/test/java/com/airwallex/android/core/log/AnalyticsLoggerTest.kt
@@ -146,7 +146,7 @@ class AnalyticsLoggerTest {
                 "merchantAppName" to "test_app",
                 "merchantAppVersion" to "1.0.1",
                 "accountId" to "aid",
-                "framework" to "android"
+                "framework" to "native"
             )
         }
 
