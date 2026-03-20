336bd528cc27252e0c17304329fd3ca256b1b928
Ferdian Sulaiman <ferdian.sulaiman@airwallex.com>
2026-03-20 17:12:00 +0800

    test: consumableEvent test

---
+++ airwallex/src/test/java/com/airwallex/android/view/util/ConsumableEventTest.kt
@@ -0,0 +1,17 @@
+package com.airwallex.android.view.util
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ConsumableEventTest {
+    @Test
+    fun `getContentIfNotHandled does not affect peekContent`() {
+        val event = ConsumableEvent(true)
+        assertEquals(true, event.peekContent())
+        assertEquals(true, event.getContentIfNotHandled())
+        assertEquals(true, event.peekContent())
+        assertNull(event.getContentIfNotHandled())
+        assertEquals(true, event.peekContent())
+    }
+}
+
